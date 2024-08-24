04cfb4c83193009830437d24e1138bd00a5f6d83
Ayush Raina <ayushraina.2013@gmail.com>
2024-08-23 17:34:37 -0700

    minor edits to base statement for gpt_api

--- gpt_api.py
+++ gpt_api.py
@@ -7,13 +7,23 @@ class OpenAIBot():
     def __init__(self):
         """
         Define openai agent and give prompt examples.
+        Make this specific to the escape room challenge
         """
         self.client = OpenAI(
             organization="your_org?",
             api_key="your_key?",
         ) 
-        self.sys_msg = """You're an assistant to answer questions.
-        """
+        self.initalization = "You're an embodied agent trapped in a simulated environment game popularly known as an escape room. \
+            You will be provided with a background story about the escape room which includes hints to what needs to be done within the room such that you can escape. \
+            You are able to see, move around, and interact with objects around you. However, you cannot be violent or break things. \
+            The format of this interactive game is iterative, where you are provided information about the objects around and you need to send back a single \
+            line of instruction of what you would do in the situation."
+        self.sys_msg = """You are an agent trapped in an escape room, use the observation and history clues wot figure out your next move. You can do the following:
+            1. Move to <location name>
+            2. Pick up <nearby object name>
+            3. ....
+            """
+        
  
    
     def __call__(self, text):
