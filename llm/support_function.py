56f56d8bf5c3f73edb7a90cd9c172d9ac9312cf7
ayush raina <ayushraina@ayushs-MacBook-Pro.local>
2024-09-20 22:54:59 +0530

    added support function and completed testing on the demo outputs

--- llm/support_function.py
+++ llm/support_function.py
@@ -1,5 +1,7 @@
 '''snipped of code taken from LLM-Planner paper. https://github.com/OSU-NLP-Group/LLM-Planner/blob/v1/hlp_planner.py'''
 
+'''reference response:
+'''
 import openai
 
 #NOTE Add your openai API key here
@@ -43,11 +45,38 @@ fixed_action_ids = {
             "step_backwards": 35, 
             "turn_left": 36, 
             "turn_right": 37}
-    
+
+def check_feasibility(action, graph):
+    """check if the action is feasible in the current environment"""
+
+    ##check if the object / parameter exists in the current graph (can be used specific to environment scene graph)
+    # graph_objects = [graph['KswainEscapeRoom2'][i]['name'] for i in range(len(graph['KswainEscapeRoom2']))]
+    # if action.split(' ')[1] not in graph_objects:
+    #     return False
+
+    ##call communication API to check feasibility in unreal engine
+
+    return True
 
 def infer_action_mapping(graph, response):
-    """this function processes LLM response to map onto feasible actions. Additionally check for feasibility from environment."""
-    return action
+    """this function processes LLM response to map onto feasible actions.
+    Exact format from the response is expected, we only process the first n line of the response.
+    Additionally check for feasibility from environment."""
+
+    assert "LLM Output" in response, "Response does not contain LLM output required for actions"
+
+    #only process llm's output related to actions
+    message_begining = "LLM Output:"
+    message_end = "\n\n" #alternatively, "{" for beginning of json
+    filtered_response = (data.split(message_begining)[1]).split(message_end)[0]
+    intended_actions = filtered_response.split("\n")
+
+    #build action set
+    #and check for feasibility
+    action_list = [action.replace('  ', '').split(' ') for action in intended_actions \
+        if action.split(' ')[0].lower() in fixed_action_ids.keys() and check_feasibility(action, graph=None)]
+
+    return action_list
 
 
 
